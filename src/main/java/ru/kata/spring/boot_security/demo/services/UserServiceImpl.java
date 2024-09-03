82d1ef2dc4811432502a82ec7bb103a8c3bc2065
Ykov91 <yakov.litvinenko.91@mail.ru>
2024-09-04 01:11:02 +0900

    Изменения в ветке Bootstrap

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -14,6 +14,8 @@ import java.util.*;
 @Service
 @Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
+
+    int x = 1;
     private final UserDAO userDAO;
     private final RoleService roleService;
     private PasswordEncoder passwordEncoder;
